c7f5f1081fb2d21cc6e1298db8966bc084dc4100
ljq <534873014@qq.com>
2019-01-15 18:12:38 +0800

    完成绑定手机号页面

--- src/pages/ChangePhone.vue
+++ src/pages/ChangePhone.vue
@@ -5,124 +5,138 @@
             手机号
         </v-title-header>
         <div class="content-wrap">
-            <img class="phone-ico" src="../assets/image/mine/phone-pic.png">
-            <div class="list">
-                <div class="item">
-                    <div class="left">
-                        绑定的手机号
-                    </div>
-                    <div class="right">{{userPhone}}</div>
-                </div>
-                <div class="middle-item">
-                    <div class="left-cell">
-                        <i></i>
-                        <div>+86</div>
-                    </div>
-                    <div class="right-cell">
-                        <input type="text" :value="userPhone">
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="left">
-                        <input type="text" placeholder="请输入您的验证码">
-                    </div>
-                    <div class="right">获取验证码</div>
-                </div>
-                <button class="confirm">确认绑定</button>
+            <div class="phone-box">
+               <div>+86</div>
+               <input type="number" placeholder="请输入手机号码" v-model="newPhone">
+               <div>中国大陆</div>
             </div>
+            <div class="verification">
+                <input type="number" placeholder="请输入验证码">
+               <div class="verification-txt">获取验证码</div>
+            </div>
+            <button class="confirm" @click="showDialogStyle=true">确定绑定</button>
+        </div>
+        <!-- 头像弹窗 -->
+        <div v-transfer-dom>
+            <x-dialog v-model="showDialogStyle" hide-on-blur :dialog-style="{'max-width': '100%', width: '100%', height: '20%', 'background-color': 'transparent'}">
+                <div class="confirm-dialog">
+                    <h2>确定换绑到下方手机号？</h2>
+                    <div class="middle-text">手机号：{{newPhone}}</div>
+                    <div class="btn-list">
+                        <button class="takePhoto">取消</button>
+                        <button class="camera">确定</button>
+                    </div>
+                </div>
+            </x-dialog>
         </div>
     </div>
 </template>
 <script>
+import {XDialog,TransferDomDirective as TransferDom} from 'vux'
 export default {
+    directives: {
+        TransferDom
+    },
     data () {
         return {
-            userPhone: "1371234555"
+            showDialogStyle: false,//弹窗显示
+            userPhone: "1371234555",
+            newPhone: "",
         }
+    },
+    created() {
+    },
+    methods: {
+
     }
 }
 </script>
 <style lang="less" scoped>
-.cell{
-    .flex-between();
-    .round(30/75rem);
-    padding: 0 40/75rem;
-    .border-box();
-    .boxshadow(0,3/75rem,6/75rem,rgba(5, 0, 0, 0.16));
-    margin: 0 auto 32/75rem auto;
-    font-size: 26/75rem;
-}
+
 .content-wrap{
-    position: absolute;
-    top: 169/75rem;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    overflow: auto;
-    .phone-ico{
-        display: block;
-        width: 238/75rem;
-        height: 216/75rem;
-        margin: 80/75rem auto;
-    }
-    .list{
-        .item{
-            width: 96%;
-            height: 90/75rem;
-            .cell();
-            input{
-                border: none;
-                outline: none;
-            }
-            .right{
-                height: 100%;
-                font-size: 26/75rem;
-                .flex-center();
-            }
+    padding: 90/75rem 57/75rem;
+    .border-box();
+    .phone-box{
+        width: 100%;
+        height: 80/75rem;
+        margin-top: 320/75rem;
+        font-size: 30/75rem;
+        .flex-between();
+        border-bottom: 1px solid rgba(238,238,238,1);
+        input{
+            width: 66%;
+            border: none;
+            padding-left: 20/75rem;
+            .border-box();
+            border-left: 1px solid rgba(221,221,221,1);
         }
-        .middle-item{
-            width: 96%;
-            height: 90/75rem;
-            .flex-between;
-            margin: 0 auto 32/75rem auto;
-            .left-cell{
-                width: 195/75rem;
-                height: 100%;
-                .cell();
-                margin: 0;
-                & > i{
-                    display: inline-block;
-                    width: 28/75rem;
-                    height: 44/75rem;
-                    background: url('../assets/image/mine/phone-ico.png') no-repeat center center / 100% 100%;
-                }
-                & > div{
-                    font-size: 26/75rem;
-                }
-            }
-            .right-cell{
-                width: 483/75rem;
-                height: 100%;
-                .cell();
-                margin: 0;
-                & > input{
-                    width: 70%;
-                    .my-input();
-                    margin-left: 20/75rem;
-                }
-            }
+    }
+    .verification{
+        height: 80/75rem;
+        margin-top: 67/75rem;
+        .flex-end();
+        font-size: 30/75rem;
+        border-bottom: 1px solid rgba(238,238,238,1);
+        input{
+            width: 65%;
+            border: none;
+            padding-left: 20/75rem;
+            .border-box();
+            border-left: 1px solid rgba(221,221,221,1);
+        }
+        .verification-txt{
+            color: @baseBoldColor;
         }
     }
     .confirm{
-        width: 500/75rem;
-        height: 100/75rem;
+        width: 640/75rem;
+        height: 85/75rem;
+        background: @baseBoldColor;
         .my-button();
-        background: @baseColor;
-        display: block;
-        margin: 70/75rem auto;
-        .round(30/75rem);
-        color: #fff;
+        .round(50/75rem);
+        margin-top: 80/75rem;
+    }
+    .btn-list{
+        .flex-start();
+        width: 100%;
+        height: 65/75rem;
     }
 }
+.weui-dialog{
+    .flex-center();
+    .confirm-dialog{
+        width: 440/75rem;
+        margin: 0 auto;
+        .round(20/75rem);
+        background: #fff;
+        h2{
+          font-size: 34/75rem;  
+        }
+        .middle-text{
+            margin: 28/75rem auto;
+        }
+        .btn-list{
+            .flex-start();
+            width: 100%;
+            height: 65/75rem;
+            button{
+                .my-button();
+                width: 50%;
+                height: 100%;
+                background: @baseBoldColor;
+                border-top:1px solid rgba(238,238,238,1);
+            }
+            button:nth-child(1){
+                background: #fff;
+                color: #999;
+                .round(0 0 0 20/75rem);
+            }
+            button:nth-child(2){
+                .round(0 0 20/75rem 0);
+            }
+        }
+    }
+}
+
 </style>
 
