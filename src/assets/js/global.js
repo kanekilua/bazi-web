a18d7bfd4563670c7510afab6922aab55bc3c957
Kane <kiluahaibin@gmail.com>
2019-01-16 14:28:11 +0800

    修复了issue:2

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -33,7 +33,7 @@ global.DATETIME_OPTION = {
 // tip message
 global.CHECK_TIP = '请同意用户协议'
 global.CHECK_ACCOUNT = '账号格式不正确'
-global.CHECK_PASSWORD = '请输入6~20位的数字与字母的集合'
+global.CHECK_PASSWORD = '密码过于简单，请重新设置密码，密码是由6-16位数字与字母的组合'
 global.CHECK_CAPTCHA = '验证码格式不正确'
 global.CHECK_PHONE = '手机号码格式不正确'
 global.CHECK_NAME = '请输入中文姓名'
