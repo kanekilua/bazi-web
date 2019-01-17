0f28f0935f04e52228cd07d1b35df1d5e45ed219
Kane <kiluahaibin@gmail.com>
2019-01-17 09:48:39 +0800

    解决issue:16

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -40,12 +40,14 @@ export default {
                     id : ""
                 }
             },
-            isCollet : null
+            isCollet : null,
+            coll_id : ''
         }
     },
     methods: {
         init : function () {
             this.cid = this.$route.query.cid;
+            this.coll_id = this.$route.params.coll_id;
             if(this.$route.query.cid === '95') {
                this.article.data.id = this.$route.query.id;
                 let params = {
@@ -98,13 +100,7 @@ export default {
             let content = res.data.content;
             if(res.data.img !== null) {
                 let Img = res.data.img.split(' ');
-                let reg = /src="http:\/\/www([^"]+)|src="https:\/\/www([^"]+)/gi;  //匹配src="http://www.zhouyi.cc或者src="https://www.zhouyi.cc
-                let srcArr = res.data.content.match(reg);
-                for(let i=0; i<srcArr.length; i++){
-                    content = content.replace(/(<\/?a.*?>)|(<\/?span.*?>)/g, '');//过滤a标签
-                    srcArr[i] ='src='+'"'+global.APP_DOMIAN+Img[i];//拼接服务器图片地址
-                    content = content.replace(/src="http:\/\/www([^"]+)|src="https:\/\/www([^"]+)/,srcArr[i])//替换图片url
-                };
+                content = content.replace(/(<\/?a.*?>)|(<\/?span.*?>)/g, '');//过滤a标签
             }
             this.article.data.content = content;
         },
@@ -128,7 +124,7 @@ export default {
         collect : function ()  {
             if(this.isCollet) {
                 let params = {
-                    coll_id : this.$route.params.coll_id
+                    coll_id : this.coll_id
                 }
                 let header = {
                     'Authorization' : localStorage.getItem(global.APP_TOKEN)
@@ -155,7 +151,7 @@ export default {
         collectSuccess : function (res) {
             if(res.code === "success") {
                 this.isCollet = true;
-                console.log('123');
+                this.coll_id = res.data.coll_id;
             }
         }
     }
